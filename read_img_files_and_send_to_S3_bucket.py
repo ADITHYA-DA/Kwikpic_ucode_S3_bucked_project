c821ec1773f63acc3649dad2f18299ae584fa98e
Adithya <adithya.davangeri@gmail.com>
2021-05-27 00:54:35 +0530

    Added images and also created read_img_files function.

--- read_img_files_and_send_to_S3_bucket.py
+++ read_img_files_and_send_to_S3_bucket.py
@@ -1,9 +1,22 @@
+import os.path as osp
+
 class ImageHandler:
+    def __init__(self, img_path):
+        self.img_path = img_path
+
     def read_img_files(self):
-        
-    def connect_and_send_to_S3_bucket:
+        for img in os.path.listdir(img_path):
+            ucode = img.split('-')[1].strip('.jpg')
+            print(ucode)
+
+    def connect_and_send_to_S3_bucket(self):
 
 
 
 if __name__ == '__main__':
-    obj = ImageHandler()
+    img_path = osp('Images')
+
+    obj = ImageHandler(img_path)
+    
+    obj.read_img_files()
+    obj.connect_and_send_to_S3_bucket()
