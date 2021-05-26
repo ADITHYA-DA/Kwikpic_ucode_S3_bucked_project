859cf37b719213e2c85d5c7486ad2e38dca59c85
Adithya <adithya.davangeri@gmail.com>
2021-05-27 01:27:48 +0530

    Added S3 bucket logic to send img file to the served Amazon S3 bucket.

--- read_img_files_and_send_to_S3_bucket.py
+++ read_img_files_and_send_to_S3_bucket.py
@@ -1,20 +1,31 @@
 import os
+import boto3
 
 class ImageHandler:
     def __init__(self, img_path):
         self.img_path = img_path
-        self.ucode_list = []
+        self.file_name_ucode_list = []
 
     def read_img_files(self):
         # print(img_path)
-        for img in os.listdir(img_path):
-            ucode = img.split('-')[1].split('.')[0]
-            print(ucode)
-            self.ucode_list.append(ucode)
+        for full_file_name in os.listdir(img_path):
+            file_name = full_file_name.split('-')[1]
+            ucode = file_name.split('.')[0]
+            # print(ucode)
+            self.file_name_ucode_list.append(tuple(full_file_name, file_name, ucode))
 
 
     def connect_and_send_to_S3_bucket(self):
-        pass
+        s3 = boto3.resource(
+            # kwikpic_selfies
+            service_name='s3',
+            region_name='us-east-2',
+            aws_access_key_id='mykey',
+            aws_secret_access_key='mysecretkey'
+        )
+
+        for full_file_name, file_name, ucode in self.file_name_ucode_list:
+            s3.Bucket('cheez-willikers').upload_file(Filename=full_file_name, Key=file_name)
 
 
 
